0e733beb83f61405f24df7d445bd2eef2a09f831
Hendrik Weisser <hendrik.weisser@stormtherapeutics.com>
2024-06-11 16:10:46 +0100

    package: add package-level documentation (for import annotations)

---
+++ R/BiomarkerDiscoveryToolkit-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @import dplyr
+#' @import ggplot2
+#' @import graphics
+#' @import stats
+#' @import tidyr
+## usethis namespace: end
+NULL
